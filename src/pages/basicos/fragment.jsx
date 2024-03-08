9f75ce312dd156f0cae4700513d90ee89e3bd1e1
Everton B. da Silva <evertonidpbjp@gmail.com>
2024-03-08 08:17:32 -0300

    após aplicação do tabuleiro

---
+++ src/pages/basicos/fragment.jsx
@@ -0,0 +1,13 @@
+export default function () {
+    return (
+    <React.Fragment>
+             <h1> Título</h1>
+             <h2> Subtítulo</h2>
+
+    </React.Fragment>
+      
+   
+    )
+
+    
+}
